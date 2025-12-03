41f956a3595fafb45260f52c0e7891c745eb2283
KaidiK <kaidi.karrik@tktk.ee>
2025-12-03 19:14:46 +0200

    add week13

--- app.js
+++ app.js
@@ -17,6 +17,35 @@ targetImage.addEventListener('click', () => {
         targetImage.style.opacity = '1';
 
     }, 200);
-
 });
-// koodis on viga, ta loopib vaid kolme pilti, seega loopib enne viimasele pildile jõudmist?//
+
+const fnameSpan = document.querySelector("#fname");
+const lnameSpan = document.querySelector("#lname");
+const emailspan = document.querySelector("#email");
+const skillsList = document.querySelector("#skills");
+const phonespan = document.querySelector("#phone");
+const agespan = document.querySelector("#age")
+
+
+window.onload = () => {
+
+    fetch('person.json')
+    .then(response => response.json())
+    .then(data => {
+
+        fnameSpan.textContent = data.fname;
+        lnameSpan.textContent = data.lname;
+        emailspan.textContent = data.email;
+        phonespan.textContent = data.phone;
+        agespan.textContent = data.age;
+
+        for( let i = 0; i <data.skills.length; i ++ ) {
+
+        let li = document.createElement('li');
+        li.innerHTML = data.skills [i];
+
+        skillsList.appendChild(li)
+    }
+
+})
+}
